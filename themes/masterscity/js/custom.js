5a7cad302588bbf4cfc5ca210934b6932c1e9e12
Maksim <maks20000@gmail.com>
2019-11-21 13:13:28 +0300

    1

---
+++ themes/masterscity/js/custom.js
@@ -0,0 +1,77 @@
+$(document).ready(function(e){
+	
+$(".tp-add").on("click", function(e){
+ $('html,body').animate({ scrollTop: 0 }, 'slow');
+ e.preventDefault();	
+	
+});
+
+
+	
+	
+	
+	//  $(".text-service").hide('slow');
+	//  $(".text-turn").hide('slow');
+	
+$(".uk-image-link-service").hover(
+	   function(){ 
+	 // $(".text-service").show('slow');
+	  $(".uk-bg-cover-service").addClass("uk-hover");
+	   
+	   },
+       function(){ 
+	   //$(".text-service").hide('slow');
+	   $(".uk-bg-cover-service").removeClass("uk-hover");
+	   }	
+);
+
+$(".uk-image-link-turn").hover(
+	   function(){ 
+	  
+	   //  $(".text-turn").show('slow'); 
+         $(".uk-bg-cover-turn").addClass("uk-hover");
+	   },
+       function(){ 
+	
+	 //  $(".text-turn").hide('slow'); 
+	   $(".uk-bg-cover-turn").removeClass("uk-hover");
+	   }
+  	
+);
+
+var slideUpServ = {
+    distance: '150%',
+    origin: 'left',
+    opacity: null,
+	delay: 2000
+};
+
+var slideUpTurn = {
+    distance: '150%',
+    origin: 'right',
+    opacity: null,
+	delay: 2000
+};
+
+//$(".uk-cover-container").fadeInScroll();
+ScrollReveal().reveal('.serv', slideUpServ);
+ScrollReveal().reveal('.turn', slideUpTurn);
+
+
+var root = $("body");
+var child = root.children();
+root.prepend("<div class='side-nav-wrapper'></div>");
+var btn = $("#open-side-nav").click (function () {
+    $(".side-nav-wrapper").append(child);
+    root.addClass("side-nav-container")
+    $(".side-nav-container").prepend($("#nav-content"));
+    $("#nav-content").toggle();
+	root.toggleClass("open");
+	$(".side-nav-wrapper").height('100vh');
+	$(".side-nav-wrapper").toggleClass("open-nav");
+})
+           
+
+
+	
+});
